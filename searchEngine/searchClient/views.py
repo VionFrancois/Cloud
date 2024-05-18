691b59db23458088d34bb0a40333457a14ea5a19
frix <210809@umons.ac.be>
2024-05-18 11:19:42 +0200

    Fix register + ajout modeles + décontenerisation

--- searchEngine/searchClient/views.py
+++ searchEngine/searchClient/views.py
@@ -4,20 +4,8 @@ import subprocess
 import os
 import matplotlib.pyplot as plt
 from django.contrib.auth.decorators import login_required
+from .recherche import *
 
-def generate_RP(plots):
-  x = []
-  y = []
-  for row in plots:
-    x.append(float(row[0]))
-    y.append(float(row[1]))
-  plt.plot(y,x,'C1', label="VGG16");
-  plt.xlabel('Rappel')
-  plt.ylabel('Précison')
-  plt.title("R/P")
-  plt.legend()
-  plt.savefig("searchClient/static/RP.png")
-  plt.close()
 
 @login_required
 def index(request):
@@ -29,26 +17,13 @@ def index(request):
             top = form.cleaned_data['top']
             file_path = os.path.join('searchClient/static/', filename)
 
-            # Vérifie si le fichier existe avant d'exécuter Docker
             if os.path.exists(file_path):
-                # Exécuter Docker ici (à modifier selon tes besoins)
-                # command = f"docker run --rm -v $(pwd)/input_files:/input my-docker-image /input/{filename}"
-                command = f"docker run recherche:latest {filename[:-4]} {top}"
-                process = subprocess.run(command, stdout=subprocess.PIPE,shell=True, check=True)
 
-                # Supposons que Docker génère les images dans le répertoire "results"
-                print(process.stdout)
-                results = process.stdout.decode("utf-8").split("\n")[:-1]
-                index = results.index("")
-                images = results[:index]
-                plots = results[index+1:]
-                for i in range(len(plots)):
-                    plots[i] = plots[i].split(" ")
+                base_image, images, _ = search(filename[:-4], top, "VGG16")
 
-                base_image = filename
                 images.insert(0, base_image)
                 print(images)
-                generate_RP(plots)
+
                 return render(request, 'searchClient/results.html', {'images': images})
             else:
                 form.add_error('filename', "Le fichier n'existe pas.")
