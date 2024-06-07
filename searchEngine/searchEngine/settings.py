e73f3521bb986c1ae9a536720ba854fa9c85fd31
VionFrancois <78816256+VionFrancois@users.noreply.github.com>
2024-06-07 16:01:04 +0200

    Final push I swear

--- searchEngine/searchEngine/settings.py
+++ searchEngine/searchEngine/settings.py
@@ -29,7 +29,7 @@ LOGIN_REDIRECT_URL = '/searchEngine/index/'
 SECRET_KEY = 'django-insecure-dpf9d#ubl_p1&3bu=dn+@+ba=fqwly3e4m)0qht8qwuk(s%d7^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["163.172.234.166"]
 
